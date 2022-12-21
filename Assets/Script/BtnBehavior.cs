09d391df9644fff5dfd1fed71e30f862babf7c66
James Tiger <jmepaz@gmail.com>
2022-12-21 21:22:41 +0800

    Add VR Button Functionality

---
+++ Assets/Script/BtnBehavior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class BtnBehavior : MonoBehaviour, IVirtualButtonEventHandler
+{
+
+    [SerializeField] private GameObject mascot;
+    VirtualButtonBehaviour virtualBtn;
+    // Start is called before the first frame update
+    void Start()
+    {
+        virtualBtn = GetComponentInChildren<VirtualButtonBehaviour>();
+        virtualBtn.RegisterEventHandler(this);
+    }
+
+
+    void IVirtualButtonEventHandler.OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        mascot.SetActive(true);
+    }
+
+    void IVirtualButtonEventHandler.OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        mascot.SetActive(false);
+    }
+}
